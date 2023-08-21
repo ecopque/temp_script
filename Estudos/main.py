31b422037ee8257a3161b5eb8dba05a8fa2352d1
voyager2 <voyager2@voyager2.voyager2>
2023-08-21 10:53:41 -0300

    Herança Múltipla

--- Estudos/main.py
+++ Estudos/main.py
@@ -1,21 +1,13 @@
-class A:
-    atributo_a = 'Valor A'
-    def metodo(self):
-        print('A')
-class B(A): #1:
-    atributo_b = 'Valor B'
-    def metodo(self):
-        print('B')
-class C(B): #2:
-    atributo_c = 'Valor C'
-    def metodo(self):
-        super(C, self).metodo() #3:
-        print('C')
-c = C()
-print(c.atributo_a) #4:
-print(c.atributo_b) #5:
-print(c.atributo_c) #6:
-c.metodo() #7:
-# Foi mesmo?
-# Foi mesmo mesmíssimo?
-# Foi tudo ok?
+class Pai1:
+    def metodo_pai1(self):
+        print("Método do Pai 1")
+class Pai2:
+    def metodo_pai2(self):
+        print("Método do Pai 2")
+class Filha(Pai1, Pai2):
+    def metodo_filha(self):
+        print("Método da Filha")
+obj = Filha() #1:
+obj.metodo_filha() #2:
+obj.metodo_pai1() #3:
+obj.metodo_pai2()
